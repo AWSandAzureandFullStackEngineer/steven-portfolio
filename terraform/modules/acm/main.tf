72f8de02c55adb4b975a5fab00b27332a1b28b11
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-05-30 01:59:20 -0500

    fix

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -12,7 +12,21 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
 
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
